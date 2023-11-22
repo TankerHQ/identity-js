acd49508c5003dd4f6466b17d28a34607e84fc37
julien mounier <julien.mounier@doctolib.com>
2023-11-22 17:40:47 +0100

    maint: align configuration with sdk-js + bump typescript to 5.2.2

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -1,25 +1,32 @@
 const path = require('path');
 const webpack = require('webpack');
 
-const webFallback = {
-  // libsodium does not use fs nor path in browsers.
-  // These packages are referenced in node environment only
-  fs: false,
-  path: false,
-  // libsodium uses node's crypto as a fallback if it doesn't find any other secure
-  // random number generator. In our case `window.crypto` is always available
-  crypto: false,
+const webResolve = {
+  fallback: {
+    // libsodium does not use fs nor path in browsers.
+    // These packages are referenced in node environment only
+    fs: false,
+    path: false,
+    // libsodium uses node's crypto as a fallback if it doesn't find any other secure
+    // random number generator. In our case `window.crypto` is always available
+    crypto: false,
 
-  // Node.js polyfills were removed from default behavior in Webpack 5
-  // But buffer and process.nextTick are used in `readable-stream` see the README:
-  // - https://github.com/nodejs/readable-stream#usage-in-browsers
-  buffer: require.resolve('buffer/'),
-  process: require.resolve('process/browser'),
+    // Node.js polyfills were removed from default behavior in Webpack 5
+    // But buffer and process.nextTick are used in `readable-stream` see the README:
+    // - https://github.com/nodejs/readable-stream#usage-in-browsers
+    buffer: require.resolve('buffer/'),
+    process: require.resolve('process/browser'),
+  },
 };
 
 const getTsLoaders = (env) => {
   const tsLoaderCompilerOptions = {
     target: 'es2019',
+    declaration: false,
+    declarationMap:false,
+    sourceMap: false,
+    declarationDir: undefined,
+    composite: false,
     declaration: false,
     importHelpers: true,
     downlevelIteration: true,
@@ -46,10 +53,6 @@ const getTsLoaders = (env) => {
         },
       },
       include: [
-        // ws lib is es6 (it assumes the users will run it in nodejs directly)
-        /node_modules(\\|\/)ws/,
-        // supports-color is es6
-        /node_modules(\\|\/)supports-color/,
         // they use arrow functions and probably more
         /node_modules(\\|\/)query-string/,
         // they use arrow functions
@@ -97,18 +100,14 @@ const makeBaseConfig = ({ mode, target, devtool, plugins }) => {
 
     node: undefined,
     devServer: undefined,
-    resolve: {
-      extensions: ['.ts', '.js'],
-    },
     stats: {
       errorDetails: true,
     },
   };
 
   if (target === 'web') {
-    // 'es5' is necessary to support IE
-    base.target = ['web', 'es5'];
-    base.resolve.fallback = webFallback;
+    base.target = ['web', 'es2019'];
+    base.resolve = webResolve;
     base.plugins.push(
       // Node.js Polyfills were removed in Webpack 5
       new webpack.ProvidePlugin({
@@ -116,6 +115,15 @@ const makeBaseConfig = ({ mode, target, devtool, plugins }) => {
       }),
     );
   }
+
+  base.resolve = {
+    ...base.resolve,
+    alias: {
+      '@tanker/test-utils': path.resolve(__dirname, '../packages/test-utils/src/index.ts'),
+      '@tanker/identity': path.resolve(__dirname, '../packages/identity/src/index.ts'),
+    },
+    extensions: ['.ts', '.js'],
+  };
 
   return base;
 };
